8ae1ff1e7e82e078083f6e07c67fe9a4d258322a
Jairo Rojas Delgado <jrojasdelgado@bcamath.org>
2021-10-31 23:12:44 +0100

    Update general stuf

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app = includeRoutes(app, models)
 
 module.exports = app;
